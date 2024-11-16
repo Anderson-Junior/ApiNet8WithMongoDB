5756e18baf7217d262971d1f16466209184b8d2f
Anderson-Junior <ander.lemos.jr@gmail.com>
2024-11-15 21:23:12 -0300

    Initial commit

---
+++ Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,63 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(x => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Name = "Smartphone",
+                    Category = "Eletrônicos",
+                    Description = "Smartphone com 128GB de armazenamento e câmera de alta resolução",
+                    Image = "https://example.com/images/smartphone.jpg",
+                    Price = 1299.99m
+                },
+                new Product()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Name = "Fone de Ouvido Bluetooth",
+                    Category = "Acessórios",
+                    Description = "Fone de ouvido sem fio com cancelamento de ruído",
+                    Image = "https://example.com/images/fone-de-ouvido.jpg",
+                    Price = 249.99m
+                },
+                new Product()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Name = "Notebook",
+                    Category = "Informática",
+                    Description = "Notebook com 16GB de RAM e 512GB SSD",
+                    Image = "https://example.com/images/notebook.jpg",
+                    Price = 4599.99m
+                },
+                new Product()
+                {
+                    Id = ObjectId.GenerateNewId().ToString(),
+                    Name = "Smart TV 4K",
+                    Category = "Eletrônicos",
+                    Description = "TV de 55 polegadas com resolução 4K e HDR",
+                    Image = "https://example.com/images/smart-tv.jpg",
+                    Price = 3299.99m
+                }
+            };
+
+            return products;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto de Estudos .NET 8 utilizando banco de dados NoSql MongoDB
+
+## Tecnologias Utilizadas
+
+- .NET 8
+- Swagger
+- MongoDD
+- Docker
